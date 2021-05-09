c75f68bb772f76dd6ebf144d09b5931f354c8f61
DawarButt <83469893+DawarButt@users.noreply.github.com>
2021-05-09 23:13:39 +0500

    Add files via upload

---
+++ Sdata.jsx
@@ -0,0 +1,25 @@
+
+const Sdata = [
+    {
+    imgscr="https://wallpapercave.com/wp/wp4056410.jpg" ,
+    title="A Netflix Original Series",
+    sname="DARK",
+    link="https://www.netflix.com/pk/title/80100172",
+    },
+    {
+     imgscr="https://img.republicworld.com/republic-prod/stories/promolarge/xxhdpi/zcufqxirbcq5uigq_1588843507.jpeg", 
+     sname="Extra curricular",
+     title="A Netflix Original Series",
+     link="https://www.netflix.com/pk/title/80990668",
+    },
+    {
+     imgscr="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfabVZS6_T5C7WVKLPcx8JvWxTN-GxStVyzA&usqp=CAU", 
+     sname="Stranger Things",
+     title="A Netflix Original Series",
+     link="https://www.netflix.com/pk/title/80057281",
+    },
+
+
+];
+
+export default Sdata;
